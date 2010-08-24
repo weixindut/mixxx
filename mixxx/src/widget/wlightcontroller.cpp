1c0c5d0ebf586bf314ceb9b324ec2d10fb10ca3b
RJ Ryan <rryan@mixxx.org>
2010-08-24 05:50:32 -0400

    adding wlightcontroller.

---
+++ mixxx/src/widget/wlightcontroller.cpp
@@ -0,0 +1,33 @@
+#include <QtDebug>
+
+#include "defs.h"
+
+#include "widget/wlightcontroller.h"
+
+
+WLightController::WLightController(QWidget* pParent) : QWidget(pParent) {
+    m_pLightController = LightController::sInstance;
+
+    // We should have been setup after the engine
+    Q_ASSERT(m_pLightController);
+
+    setupUi(this);
+
+    QBoxLayout* box = dynamic_cast<QBoxLayout*>(layout());
+    Q_ASSERT(box); //Assumes the form layout is a QVBox/QHBoxLayout!
+    box->removeWidget(m_colorPickerPlaceholder);
+    m_colorPickerPlaceholder->hide();
+    m_pColorPicker = new QColorPicker(this);
+    box->insertWidget(1, m_pColorPicker);
+    connect(m_pColorPicker, SIGNAL(newCol(int, int)), this, SLOT(slotSetColor(int, int)));
+}
+
+WLightController::~WLightController() {
+}
+
+void WLightController::slotSetColor(int hue, int value) {
+    QColor color = QColor::fromHsv(hue, 255, value);
+    qDebug() << "Setting to hue:" << hue << "value" << value;
+    m_pLightController->setColor(color);
+}
+
---
+++ mixxx/src/widget/wlightcontroller.h
@@ -0,0 +1,29 @@
+#ifndef WLIGHTCONTROLLER_H
+#define WLIGHTCONTROLLER_H
+
+#include "ui_dlglights.h"
+
+#include <QWidget>
+
+#include "lights/lightcontroller.h"
+#include "widget/qcolorpicker.h"
+
+
+class WLightController : public QWidget, public Ui::DlgLights {
+    Q_OBJECT
+  public:
+    WLightController(QWidget *pParent);
+    virtual ~WLightController();
+
+  private slots:
+    // void on_beat();
+    // void on_onset();
+    // void on_pitch(float newPitch);
+    // void receive_fft();
+    void slotSetColor(int hue, int sat);
+  private:
+    LightController* m_pLightController;
+    QColorPicker* m_pColorPicker;
+};
+
+#endif /* WLIGHTCONTROLLER_H */
