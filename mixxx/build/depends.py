76881e25f014a961a9f987167dee08dbea7409a3
RJ Ryan <rryan@mixxx.org>
2013-05-08 10:27:52 -0400

    Roll-back r3273.

--- mixxx/build/depends.py
+++ mixxx/build/depends.py
@@ -387,10 +387,9 @@ class TagLib(Dependence):
 
 class Chromaprint(Dependence):
     def configure(self, build, conf):
-        if not conf.CheckLib(['chromaprint_p', 'libchromaprint_p']):
-            raise Exception("Could not find libchromaprint_p or its development headers.")
-        if not conf.CheckLib(['chromaprint', 'libchromaprint']):
+        if not conf.CheckLib(['chromaprint', 'libchromaprint', 'chromaprint_p', 'libchromaprint_p']):
             raise Exception("Could not find libchromaprint or its development headers.")
+
         build.env.Append(CPPPATH='/usr/include/')
 
 class ProtoBuf(Dependence):
