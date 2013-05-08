e7254a0dc43a88241e912c8c3cd1bf8d6982dd4c
RJ Ryan <rryan@mixxx.org>
2013-05-08 09:37:55 -0400

    Check for chromaprint_p.

--- mixxx/build/depends.py
+++ mixxx/build/depends.py
@@ -387,7 +387,9 @@ class TagLib(Dependence):
 
 class Chromaprint(Dependence):
     def configure(self, build, conf):
-        if not conf.CheckLib('chromaprint'):
+        if not conf.CheckLib(['chromaprint', 'libchromaprint']):
+            raise Exception("Could not find libchromaprint or its development headers.")
+        if not conf.CheckLib(['chromaprint_p', 'libchromaprint_p']):
             raise Exception("Could not find libchromaprint or its development headers.")
 
         build.env.Append(CPPPATH='/usr/include/')
