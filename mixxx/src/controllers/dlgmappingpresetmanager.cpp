425484ba4ec463b6f43cc6882df9c6a0294d4ccb
weixindut <weixindlut@gmail.com>
2013-07-03 23:43:16 +0800

    test for getting search result from web server

--- mixxx/src/controllers/dlgmappingpresetmanager.cpp
+++ mixxx/src/controllers/dlgmappingpresetmanager.cpp
@@ -13,6 +13,7 @@
 #include "controllers/dlgmappingpresetmanager.h"
 #include "controllers/httpclient.h"
 #include "controllers/json.h"
+#include "controllers/wao/presetobjectwao.h"
 using namespace QtJson;
 
 DlgMappingPresetManager::DlgMappingPresetManager(QWidget* parent)
@@ -20,17 +21,19 @@ DlgMappingPresetManager::DlgMappingPresetManager(QWidget* parent)
     m_ui.setupUi(this);
 }
 void DlgMappingPresetManager::getJsonDataTest() {
-    HttpClient httpclient;
-    bool ok;
-    QString data = httpclient.get("http://127.0.0.1:8000/api/v1/midi/company/?format=json");
-    qDebug() << "Print JsonObject:"+data;
+	PresetObjectWAO pow;
+    pow.getPresetByName("vmpk");
+    //HttpClient httpclient;
+    //bool ok;
+    //QString data = httpclient.get("http://127.0.0.1:8000/api/v1/midi/company/?format=json");
+    //qDebug() << "Print JsonObject:"+data;
 
-    QVariantMap result = QtJson::parse(data,ok).toMap();
-    if(!ok) {
-        exit(1);
-    }
-    foreach(QVariant plugin, result["objects"].toList()) {
-        QVariantMap res = plugin.toMap();
-        qDebug() <<"print company name:"+res["company_name"].toString();
-    }
+    //QVariantMap result = QtJson::parse(data,ok).toMap();
+    //if(!ok) {
+    //    exit(1);
+    //}
+    //foreach(QVariant plugin, result["objects"].toList()) {
+    //    QVariantMap res = plugin.toMap();
+    //    qDebug() <<"print company name:"+res["company_name"].toString();
+    //}
 }
